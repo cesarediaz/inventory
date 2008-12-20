07643f8a887f0ec2d88af1c4682115e2502072f5
cesar diaz <cesare.diaz@gmail.com>
2008-12-20 16:31:41 -0200

    - Model   * validate unique names, ip and mac

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,5 +1,6 @@
 class Computer < ActiveRecord::Base
 
+  validates_uniqueness_of :name, :ip, :mac
   validates_presence_of :name, :ip, :mac
   validates_size_of :ip, :within => 8..15
   validates_format_of :ip, :with => /^\b(?:\d{1,3}\.){3}\d{1,3}\b/i
