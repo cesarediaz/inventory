8981f8c510d16b86fbd182d075e316f77ba18324
cesar diaz <cesare.diaz@gmail.com>
2008-12-21 15:57:57 -0200

    - Model   * added regex validation for mac field

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -4,5 +4,5 @@ class Computer < ActiveRecord::Base
   validates_presence_of :name, :ip, :mac
   validates_size_of :ip, :within => 8..15
   validates_format_of :ip, :with => /^\b(?:\d{1,3}\.){3}\d{1,3}\b/i
-
+  validates_format_of :mac, :with => /^([0-9a-f]{2}([:-]|$)){6}$/i
 end
