4c531c55a3d20ca746a745e68e6216a5d0cb82fa
cesar diaz <cesare.diaz@gmail.com>
2008-12-21 17:12:10 -0200

    - Model   * added validations
    - Partials
      * add form partial to motherboars
      * add menu partial in shared folder inside views

--- app/models/mother_board.rb
+++ app/models/mother_board.rb
@@ -1,2 +1,5 @@
 class MotherBoard < ActiveRecord::Base
+  validates_uniqueness_of :serialnumber
+  validates_presence_of :title, :serialnumber
+  validates_size_of :serialnumber, :within => 8..15
 end
