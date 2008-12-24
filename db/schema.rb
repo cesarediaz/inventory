7d5845abf33cb10eb709bec6eaeafae40a33c440
cesar diaz <cesare.diaz@gmail.com>
2008-12-24 16:28:59 -0200

    - View andartial   * added available field in index view   * added fieldset tag in form

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081224172121) do
+ActiveRecord::Schema.define(:version => 20081224174008) do
 
   create_table "cds", :force => true do |t|
     t.string   "model"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20081224172121) do
     t.string   "mac"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "available",  :default => true
   end
 
   create_table "dvds", :force => true do |t|
