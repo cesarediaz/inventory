87df13a68cf541ead3b630ecdeb422a52884fb78
cesar diaz <cesare.diaz@gmail.com>
2008-12-26 18:46:11 -0200

    - Model   * add model mark
    - Partills
      * little changes in forms
    
    - Views
      * little changes

---
+++ app/models/mark.rb
@@ -0,0 +1,12 @@
+class Mark < ActiveRecord::Base
+
+  has_many :mother_board, :dependent => :nullify
+  has_many :harddisk, :dependent => :nullify
+  has_many :memory, :dependent => :nullify
+  has_many :cd, :dependent => :nullify
+  has_many :dvd, :dependent => :nullify
+
+  #################################################
+  # VALIDATIONS
+  validates_uniqueness_of :name
+end
