6c29e3d6520aff090787b1e6cf889cf19bea04b2
cesar diaz <cesare.diaz@gmail.com>
2008-12-27 23:02:32 -0200

    - Helpers and View   * little changes

--- app/helpers/computers_helper.rb
+++ app/helpers/computers_helper.rb
@@ -1,8 +1,10 @@
 module ComputersHelper
 
-  def url_google_graph(availables, unavailables)
+  def url_google_graph_pie_3d(availables, unavailables)
 
-    link_to 'Graphic Stats',  GoogleChart.pie_3d_350x150('availables'.capitalize => availables,'unavailables'.capitalize => unavailables).to_url
+    link_to 'Graphic Stats',
+    GoogleChart.pie_3d_350x100('availables'.upcase => availables,
+                               'unavailables'.upcase => unavailables).to_url, options = { :popup => true }
 
   end
 
