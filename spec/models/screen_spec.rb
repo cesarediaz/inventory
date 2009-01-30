6a19b053aa707c858106c1c12aac72faba37cd1b
cesar diaz <cesare.diaz@gmail.com>
2009-01-30 19:07:15 -0200

    - Specs   * added specs of screen

---
+++ spec/models/screen_spec.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Screen do
+  before(:each) do
+  end
+
+  it "should create a new instance given valid attributes" do
+    screen = Screen.create({:model => 'SyncMaster 3v', :serialnumber => 'a123456' })
+    screen.should be_valid
+    screen.errors.should be_empty
+  end
+
+  it "should not create a new instance given invalid attributes without model" do
+    screen = Screen.create({:model => nil, :serialnumber => '1234567890' })
+    screen.should have(1).errors_on(:model)
+  end
+
+  it "should not create a new instance given two equals attributes of serial number" do
+    screen1 = Screen.create({:model => 'SyncMaster 3v', :serialnumber => '1234567890' })
+    screen2 = Screen.create({:model => 'SyncMaster 3v', :serialnumber => '1234567890' })
+    screen2.should have(1).errors_on(:serialnumber)
+  end
+
+end
