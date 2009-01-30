e4830d036965510ceaa6a3c30f509fa8dba41858
cesar diaz <cesare.diaz@gmail.com>
2009-01-30 19:12:57 -0200

    - Specs   * added new model spec printer

---
+++ spec/models/printer_spec.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Printer do
+  before(:each) do
+  end
+
+  it "should create a new instance given valid attributes" do
+    printer = Printer.create({:model => 'HP 800', :serialnumber => 'a123456' })
+    printer.should be_valid
+    printer.errors.should be_empty
+  end
+
+  it "should not create a new instance given invalid attributes without model" do
+    printer = Printer.create({:model => nil, :serialnumber => '1234567890' })
+    printer.should have(1).errors_on(:model)
+  end
+
+  it "should not create a new instance given two equals attributes of serial number" do
+    printer1 = Printer.create({:model => 'HP 800', :serialnumber => '1234567890' })
+    printer2 = Printer.create({:model => 'Laser 1020', :serialnumber => '1234567890' })
+    printer2.should have(1).errors_on(:serialnumber)
+  end
+
+end
