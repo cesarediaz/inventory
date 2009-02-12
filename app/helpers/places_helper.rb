e1432f81afe07049729b23c5b7009ac6353668de
cesar diaz <cesare.diaz@gmail.com>
2009-02-12 10:46:41 -0200

    - Helper   * deleted method not necesary

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,9 +1,2 @@
 module PlacesHelper
-  def url_google_graph_pie_3d(hash)
-
-    link_to 'Graphic Stats',
-    GoogleChart.pie_3d_350x100(hash).to_url,
-    options = { :popup => true }
-
-  end
 end
