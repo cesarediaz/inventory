acd8b41be297c3978ee95c4d75cf658d3a46409d
cesar diaz <cesare.diaz@gmail.com>
2009-02-18 11:46:50 -0200

    - Helpers   * added new method 'advice'
    - Partials
      * addapted forms to show an advice when the device is in
        a workstation
    
    - Locales
      * added new translations
    
    - Css
      * added new styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def advice(object)
+    html = ''
+   if  #{object}
+     html = html + '<div id="advice-quarter" >'
+     html = html + "<a href=\"#\" onclick=\"Effect.toggle('advice-quarter', 'appear'); return false;\">"
+     html = html +  t('common-actions.hide-advice') + '</a>'
+     html = html +  '<br>'
+     html = html +  t('help-phrases.in-workstation')
+     html = html +  '</div>'
+   end
+    return html
+
+  end
+
 end
