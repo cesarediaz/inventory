3683dd74c9528d6c3e21d139b8ca39ca83b54c07
cesar <cesare.diaz@gmail.com>
2009-02-25 07:37:51 -0200

    - Helper   * added comments to existent code   * added new method in_workstation?()
    - Partial
      * addapted calling new helpers methods
    
    - Model
      * deleting restrictiong of uniques printer for
        workstations model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  # Take and object and depending if it is true make a help advice
+  #
+  # Return: html
   def advice(object, head, message)
     html = ''
     if  #{object}
@@ -15,10 +18,31 @@ module ApplicationHelper
 
   end
 
+  #Get a value and depending of the value return a phrase
+  #
+  #Return: - yes if value is true
+  #        - no if value is false
   def check(value)
     html = ''
     if value
       html = html +  t('phrases.y')
+    else
+      html = html +  t('phrases.n')
+    end
+    return html
+  end
+
+  #Get a value and depending of the value return a link or a phrase
+  #
+  #Return: - link if value is true
+  #        - no if value is false
+  def in_workstation?(value)
+    html = ''
+    if value.is_part_of_a_workstation
+      html = html +  link_to(t('phrases.y'), :controller => 'workstations',
+                             :action => 'show',
+                             :id => Workstation.find(:first,
+                                                     :conditions => [ "computer_id = ?", value.id]).id)
     else
       html = html +  t('phrases.n')
     end
--- app/models/workstation.rb
+++ app/models/workstation.rb
@@ -5,7 +5,7 @@ class Workstation < ActiveRecord::Base
 
   #################################################
   # VALIDATIONS
-  validates_uniqueness_of :computer_id, :screen_id, :printer_id
+  validates_uniqueness_of :computer_id, :screen_id
   validates_presence_of :computer_id, :screen_id
 
   private
