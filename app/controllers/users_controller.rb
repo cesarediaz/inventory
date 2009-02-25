88d9c5938b3168ff08bb942478c2f2e0d8de1f5e
cesarediaz <cesare.diaz@gmail.com>
2009-02-25 19:29:28 +0300

    - Controllers   * added edit method in users controller   * added two private methods, load_user and logged_in
    - Helpers
      * added method is_admin? to check the user level
    
    - Partials
      * added options to edit user account and generate
        a new account
    
    - Views
      * changes in new and edit views for users
    
    - Locales
      * added translations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   include AuthenticatedSystem
   layout "primary-content"
 
-  # render new.rhtml
   def new
     @user = User.new
   end
@@ -13,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     success = @user && @user.save
     if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
+      # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
@@ -25,4 +24,36 @@ class UsersController < ApplicationController
       render :action => 'new'
     end
   end
+
+  def edit
+    begin
+      logged_in
+    rescue ActiveRecord::RecordNotFound
+      redirect_to ('/')
+    end
+  end
+
+  def update
+    load_user
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to('/') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  protected
+
+  def load_user
+    @user = User.find(params[:id]) if logged_in?
+  end
+
+  def logged_in
+    logged_in? ? load_user : redirect_to('/')
+  end
+
 end
