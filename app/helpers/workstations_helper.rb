c3d12bf44bbde28548db252f36ab6535795318c2
cesar <cesare.diaz@gmail.com>
2009-02-25 11:15:57 -0200

    - Helpers   * refactoring statistics helper method in workstations
    - Partials
      * added partial for small resumes of statistics
      * refactoring stats partial

--- app/helpers/workstations_helper.rb
+++ app/helpers/workstations_helper.rb
@@ -1,16 +1,28 @@
 module WorkstationsHelper
 
-  def statistics(object, id)
+  #Generate html showing small stats resumes of devices
+  #
+  #Params: label  data label
+  #        object model objet
+  #        place  id reference of place
+  #
+  #Return: html
+  def statistics(label, object, place)
     html = ''
     html = html + "<div id='stats_data'>"
-    html = html +  t('workstations.computer') + ':' +  #{object}.list_for_place(id).count.to_s rescue nil + t('phrases.units')
+    eval %" @total = #{object}.list_for_place(place).count.to_s rescue nil ";
+    html = html +  label + ':' +  @total rescue nil  + t('phrases.units')
     html = html +  '</div>'
 
-    html = html + "<div id='stats_data_centered'>"
-    html = html + t('stats.alone') + ':' + #{object}.list_for_place_are_not_part_a_workstation(id).count.to_s rescue nil + t('phrases.units')
+    eval %" @out = #{object}.list_for_place_are_not_part_a_workstation(place).count.to_s rescue nil ";
+    html = html + "<div place='stats_data_centered'>"
+    html = html + t('stats.alone') + ':' + @out rescue nil + t('phrases.units')
     html = html + '</div>'
+
+
+    eval %" @in = #{object}.list_for_place_as_part_a_workstation(id).count.to_s rescue nil";
     html = html +  "<div id='stats_data_centered'>"
-    html = html +  t('stats.workstation') + ':' + #{object}.list_for_place_as_part_a_workstation(id).count.to_s rescue ni +  "t('phrases.units')"
+    html = html +  t('stats.workstation') + ':' + @in rescue nil +  "t('phrases.units')"
     html = html +  '</div>'
 
     return html
