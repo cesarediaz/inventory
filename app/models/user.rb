77a7f5a300dd7ab9c07078bb035ac9caa09923cd
cesar <cesare.diaz@gmail.com>
2009-02-26 11:48:15 -0200

    - Model   * added attr_accessible is_admin
    - Views
      * added is_admin field in new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation, :language
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :language, :is_admin
 
 
 
