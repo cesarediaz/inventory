a7c62b470334d85e90351afacc0e42ca52c6efba
cesar <cesare.diaz@gmail.com>
2009-02-27 14:04:22 -0200

    - Controllers   * changed create method to don't go logged in with a new     user account when it is created
    - Views
      * the admin account can not change its admin status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,16 +20,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    logout_keeping_session!
+    #logout_keeping_session! # !! not logout
+
     @user = User.new(params[:user])
     success = @user && @user.save
     if success && @user.errors.empty?
+
       # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
-      self.current_user = @user # !! now logged in
-      redirect_back_or_default('/')
+      #self.current_user = @user # !! now NOT logged in
+
+      redirect_back_or_default('/users/list')
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
