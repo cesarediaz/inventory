cbab32b05d067c4e40944ecd4dbf9421178a114a
cesar <cesare.diaz@gmail.com>
2009-03-02 10:20:58 -0200

    - Library   * added format to the report   * added comments in pdf reporter library

--- lib/pdf_report_system.rb
+++ lib/pdf_report_system.rb
@@ -1,10 +1,24 @@
 module PdfReportSystem
   protected
 
-  def pdf_report(elements, paper, title, font, columns_order)
+  #This method generate a pdf file to download
+  #
+  #Parameters:
+  # elements: data to fill the pdf to download
+  # paper: type of paper to generate the printable file(e.g. A4,
+  #        LETTER and so on)
+  # title: title of the pdf file report
+  # font_size: size of the letters in the file
+  # columns_order: order that will be display the columns in the
+  # pdf file if this file will show a table with data
+  #
+  #Return a .pdf file with the report
+  def pdf_report(elements, paper, title, font_size, columns_order)
     pdf = PDF::Writer.new(:paper => paper)
     pdf.select_font "Times-Roman"
-    pdf.text title, :font_size => font, :justification => :center
+
+    pdf.text title, :font_size => font_size, :justification => :center,
+    :spacing => 2
 
     PDF::SimpleTable.new do |tab|
       columns_places_pdf_report(tab, columns_order)
@@ -21,35 +35,50 @@ module PdfReportSystem
     send_data pdf.render, :filename => title, :type => 'application/pdf'
   end
 
+  #Report of places
+  #
+  #Parameters:
+  # tab: object to add tab columns in the pdf file report
+  # columns_order: order of the columns
+  #
+  #Return the content of the pdf file report
   def columns_places_pdf_report(tab, columns_order)
-      tab.column_order = columns_order
+    tab.column_order = columns_order
+    tab.text_color = Color::RGB::Black
+    tab.show_lines = :none
 
       tab.columns["col1"] = PDF::SimpleTable::Column.new("col1") { |col|
-        col.width = 100
+        col.width = 200
         col.heading = t('places.description')
       }
 
       tab.columns["col2"] = PDF::SimpleTable::Column.new("col2") { |col|
-        col.width = 70
+        col.width = 80
         col.heading = t('places.description')
       }
 
       tab.columns["col3"] = PDF::SimpleTable::Column.new("col2") { |col|
-        col.width = 70
+        col.width = 80
         col.heading = t('places.computer')
       }
 
       tab.columns["col4"] = PDF::SimpleTable::Column.new("col4") { |col|
-        col.width = 70
+        col.width = 80
         col.heading = t('places.screen')
       }
 
       tab.columns["col5"] = PDF::SimpleTable::Column.new("col5") { |col|
-        col.width = 70
+      col.width = 80
         col.heading = t('places.printer')
       }
   end
 
+  #Data to fill each column in the report
+  #
+  #Parameters:
+  # content: content to write in the pdf file
+  #
+  #Return the data loaded in each row of the places report
   def data_places_report(content)
     { "col1" => "#{content.title}", "col2" => "#{content.description}" \
         , "col3" => "#{content.computer.count}", "col4" => "#{content.screen.count}" \
