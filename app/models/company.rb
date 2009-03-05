8af9780c44b6f708be37273855741005888ae3b9
cesar <cesare.diaz@gmail.com>
2009-03-05 11:35:38 -0200

    - Model   * added control in email format

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,6 @@ class Company < ActiveRecord::Base
   # VALIDATIONS
   validates_uniqueness_of :name
   validates_presence_of :name, :address, :phone, :fax, :email
+  validates_format_of :email, :with =>
+    %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]+)*(\.[a-z]{2,4})$}i
 end
