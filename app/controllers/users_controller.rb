2585ed6469da05f7cf8085684f6e14c421f0cb3d
cesar <cesare.diaz@gmail.com>
2009-03-05 11:25:38 -0200

    - Controllers   * added destroy method in users controllers
    - View and partial
      * added translation
    
    - Locales
      * added translations for company module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(list_users_url) }
+      format.xml  { head :ok }
+    end
+  end
+
   protected
 
   def load_user
