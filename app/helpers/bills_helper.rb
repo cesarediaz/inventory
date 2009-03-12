17312f966a68d90e26fd65ef0cb74ac80a9db5cf
cesar <cesare.diaz@gmail.com>
2009-03-12 16:22:38 -0200

    - Helpers   * added new method which return a html
    - View
      * refactoring
    
    - Css
      * added new styles for bills

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,2 +1,16 @@
 module BillsHelper
+
+  def show_data(data, object, label)
+    html = ''
+
+    if object > 0
+      html = html + content_tag('label', label,  :id => "bill-content" )
+      html = html + content_tag('ul', data.collect {|x| x.model.to_s + \
+                                  ' ' + x.mark.name + \
+                                  ' ' + x.serialnumber \
+                                  + '<br>' },  :id => "bill-content-decription" )
+    end
+    return html
+  end
+
 end
