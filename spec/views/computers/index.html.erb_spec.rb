8ac66188aedbe39db04be4801425d13ff00f99bc
cesar <cesare.diaz@gmail.com>
2009-04-09 17:05:46 -0300

    - Spec   * added spec to show link Show/Hide in search partial
    - Partial
      * refactoring

--- spec/views/computers/index.html.erb_spec.rb
+++ spec/views/computers/index.html.erb_spec.rb
@@ -9,12 +9,28 @@ describe "computers/show.html.erb" do
                            :mac => '13:23:43:15:53:30',
                            :inventory_register => '23429342',
                            :available => true)
+
     Computer.stub!(:find).and_return(@computer)
     assigns[:computer] = @computer
+
+    @computer_one = mock_model(Computer, :name => "pc_1", :ip => '132.54.23.60', :mac => '13:23:43:15:53:30',
+                           :available => true)
+    @computer_two = mock_model(Computer, :name => "pc_2", :ip => '132.54.23.61', :mac => '13:23:43:15:53:31',
+                           :available => false)
+    @computers = [@computer_one, @computer_two]
+    assigns[:computers] = @computers
   end
 
   it "should display the text of the place" do
     render "computers/show.html.erb"
     assert_select 'p', 'Place'
   end
+
+ end
+ describe "computers/_search.html.erb" do
+  it "should display the text link to Show/Hide the search" do
+    render "computers/_search.html.erb"
+    assert_select 'a', 'Show/Hide'
+  end
+
 end
