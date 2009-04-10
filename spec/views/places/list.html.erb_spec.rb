0ba70a856d1dd0bacc548e6e800e74216579e981
cesar <cesare.diaz@gmail.com>
2009-04-10 15:47:25 -0300

    - Spec View   * added new spec for list in places module

---
+++ spec/views/places/list.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper' )
+
+describe "places/_links_files_downloads.html.erb" do
+  before(:each) do
+    render "places/_links_files_downloads.html.erb"
+  end
+
+  it "should display two strings to download xls and pdf files" do
+    assert_select 'a', 'Download a xls file'
+    assert_select 'a', 'Download a pdf file'
+  end
+ end
+
+describe "places/_selector_places_list.html.erb" do
+  before(:each) do
+    render "places/_selector_places_list.html.erb"
+  end
+
+  it "should display text for select" do
+    assert_select 'label', 'Select diferents places for description'
+  end
+
+  it "should display a select tag with six options" do
+    assert_select "select" do |elements|
+      elements.each do |element|
+        assert_select element, "option", 6
+      end
+    end
+  end
+
+ end
