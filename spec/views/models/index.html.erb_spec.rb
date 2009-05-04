f5f43139319955f890c70b54820834f11a41ee29
cesar <cesare.diaz@gmail.com>
2009-05-04 10:16:27 -0300

    - Spec   * added new test for views

--- spec/views/models/index.html.erb_spec.rb
+++ spec/views/models/index.html.erb_spec.rb
@@ -6,3 +6,30 @@ describe "/models/index" do
     render "/models/index"
   end
 end
+
+
+describe "/models/index.html.erb" do
+
+  before do
+    @models = Model.find(:all)
+    assigns[:models] = @models
+    render '/models/index.html.erb'
+  end
+
+  it "should display link to new model" do
+    response.should have_tag("a[href='/models/new']") do
+    end
+  end
+
+  it "should display table" do
+    response.should have_tag("table") do
+      assert_select "table" do |elements|
+        elements.each do |element|
+          assert_select element, "th", 4
+        end
+      end
+    end
+  end
+
+
+end
