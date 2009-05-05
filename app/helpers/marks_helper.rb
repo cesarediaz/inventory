648d5e2b6126b199d328862123d8ef8b6c83259a
cesarediaz <cesare.diaz@gmail.com>
2009-05-05 18:39:23 +0300

    - Helper   * adding some logic inside
    - View
      * adding some logic there too

--- app/helpers/marks_helper.rb
+++ app/helpers/marks_helper.rb
@@ -1,9 +1,11 @@
 module MarksHelper
   def models(mark)
-    content_tag :div, options = {:class => 'background_simple_lists'} do
-      mark.model.collect {|x|
-        content_tag('div', x.description)
-      }
+    if not mark.model.empty?
+      content_tag :div, options = {:class => 'background_simple_lists'} do
+        mark.model.collect {|x|
+          content_tag('div', x.description)
+        }
+      end
     end
   end
 end
