e971897a933554c8e36633b5d3f332c3f132cd9e
cesarediaz <cesare.diaz@gmail.com>
2009-05-05 19:03:37 +0300

    - Helper   * refactoring
    - View
      * refactoring

--- app/helpers/marks_helper.rb
+++ app/helpers/marks_helper.rb
@@ -3,7 +3,7 @@ module MarksHelper
     if not mark.model.empty?
       content_tag :div, options = {:class => 'background_simple_lists'} do
         mark.model.collect {|x|
-          content_tag('div', x.description)
+          content_tag('h4', x.description)
         }
       end
     end
