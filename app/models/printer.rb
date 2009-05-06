399d11d0504215e281f68a9bf7054a9a7b085b50
cesar <cesare.diaz@gmail.com>
2009-05-06 15:26:15 -0300

    - Models   * added methods to get the model description for     each printer and screen
    - Partials
      * addapted
    
    - Fixtures
      * fixed

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -16,4 +16,9 @@ class Printer < ActiveRecord::Base
       :conditions => ['place_id = ? and is_part_of_a_workstation = ?', args, false]}}
   named_scope :list_for_place_as_part_a_workstation, lambda { |*args| {
       :conditions => ['place_id = ? and is_part_of_a_workstation = ?', args, true]}}
+
+  def description_model
+    "#{self.mark.name} #{self.model.description}" unless self.model_id.nil?
+  end
+
 end
--- app/models/screen.rb
+++ app/models/screen.rb
@@ -17,4 +17,9 @@ class Screen < ActiveRecord::Base
   named_scope :list_for_place_as_part_a_workstation, lambda { |*args| {
       :conditions => ['place_id = ? and is_part_of_a_workstation = ?', args, true]}}
 
+
+  def description_model
+    "#{self.mark.name} #{self.model.description}" unless self.model_id.nil?
+  end
+
 end
