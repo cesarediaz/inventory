1fbcd82c3f697217f00929ef0dc09cd21193ed33
cesarediaz <cesare.diaz@gmail.com>
2009-05-08 18:39:42 +0300

    - Helper   * refactoring

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -2,11 +2,9 @@ module BillsHelper
 
   def show_data(data, object, label)
     html = ''
-
     if object > 0
       html = html + content_tag('label', label,  :id => "bill-content" )
-      html = html + content_tag('ul', data.collect {|x| x.model.to_s + \
-                                  ' ' + x.mark.name + \
+      html = html + content_tag('ul', data.collect {|x| x.description_model + \
                                   ' ' + x.serialnumber \
                                   + '<br>' },  :id => "bill-content-decription" )
     end
