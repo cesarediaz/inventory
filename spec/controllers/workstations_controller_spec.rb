c0ef50aa383d2131b9333c63fcb756ccb76b37f5
cesar <cesare.diaz@gmail.com>
2009-05-13 15:14:11 -0300

    - Spec   * refactoring

--- spec/controllers/workstations_controller_spec.rb
+++ spec/controllers/workstations_controller_spec.rb
@@ -17,32 +17,34 @@ describe ComputersController do
     controller.stub!(:set_user_language).and_return('en')
   end
 
-  before do
-  end
 
-  def do_get
-    get :new, :place_id => "2"
-  end
+  describe "get list of computers, screens and printers" do
+    before do
+    end
 
-  it "for this place it should have n computers" do
-    login
-    do_get
-    @computers = Computer.list_for_place_are_not_part_a_workstation(params[:place_id])
-    @computers.should have(11).items
-  end
+    def do_get
+      get :new, :place_id => "2"
+    end
 
-  it "for this place it should have n screens" do
-    login
-    do_get
-    @screens = Screen.list_for_place_are_not_part_a_workstation(params[:place_id])
-    @screens.should have(11).items
-  end
+    it "for this place it should have n computers" do
+      login
+      do_get
+      @computers = Computer.list_for_place_are_not_part_a_workstation(params[:place_id])
+      @computers.should have(11).items
+    end
 
-  it "for this place it should have n printers" do
-    login
-    do_get
-    @printers = Printer.list_for_place_are_not_part_a_workstation(params[:place_id])
-    @printers.should have(3).items
-  end
+    it "for this place it should have n screens" do
+      login
+      do_get
+      @screens = Screen.list_for_place_are_not_part_a_workstation(params[:place_id])
+      @screens.should have(11).items
+    end
 
+    it "for this place it should have n printers" do
+      login
+      do_get
+      @printers = Printer.list_for_place_are_not_part_a_workstation(params[:place_id])
+      @printers.should have(3).items
+    end
+  end
 end
