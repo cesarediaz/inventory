5205f56a8ad7dcb36cf79eaa0c6fbd494f3cc7e2
cesar <cesare.diaz@gmail.com>
2009-05-13 15:23:09 -0300

    - Spec   * refactoring of workstations controller spec

--- spec/controllers/workstations_controller_spec.rb
+++ spec/controllers/workstations_controller_spec.rb
@@ -22,27 +22,52 @@ describe ComputersController do
     before do
     end
 
-    def do_get
+    def do_get_for_office_one
+      get :new, :place_id => "1"
+    end
+
+    def do_get_for_office_two
       get :new, :place_id => "2"
     end
 
-    it "for this place it should have n computers" do
+    it "for  office one it should have n computers" do
       login
-      do_get
+      do_get_for_office_one
+      @computers = Computer.list_for_place_are_not_part_a_workstation(params[:place_id])
+      @computers.should have(1).items
+    end
+
+    it "for office two it should have n computers" do
+      login
+      do_get_for_office_two
       @computers = Computer.list_for_place_are_not_part_a_workstation(params[:place_id])
       @computers.should have(11).items
     end
 
-    it "for this place it should have n screens" do
+    it "for office one it should have n screens" do
       login
-      do_get
+      do_get_for_office_one
+      @screens = Screen.list_for_place_are_not_part_a_workstation(params[:place_id])
+      @screens.should have(0).items
+    end
+
+    it "for office two it should have n screens" do
+      login
+      do_get_for_office_two
       @screens = Screen.list_for_place_are_not_part_a_workstation(params[:place_id])
       @screens.should have(11).items
     end
 
-    it "for this place it should have n printers" do
+    it "for office one it should have n printers" do
       login
-      do_get
+      do_get_for_office_one
+      @printers = Printer.list_for_place_are_not_part_a_workstation(params[:place_id])
+      @printers.should have(0).items
+    end
+
+    it "for office two it should have n printers" do
+      login
+      do_get_for_office_two
       @printers = Printer.list_for_place_are_not_part_a_workstation(params[:place_id])
       @printers.should have(3).items
     end
