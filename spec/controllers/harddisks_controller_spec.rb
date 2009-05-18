c67f58d7ffac11d1aab7190a43afac09276e9380
cesar <cesare.diaz@gmail.com>
2009-05-18 16:27:28 -0300

    - Controllers   * added spec for hard disks module
    - Fixtures
      * fixed yml for hard disks module

---
+++ spec/controllers/harddisks_controller_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+# Be sure to include AuthenticatedTestHelper in spec/spec_helper.rb instead
+# Then, you can remove it from this and the units test.
+
+describe HarddisksController do
+  fixtures :harddisks
+
+  describe "get list of harddisks" do
+    before do
+    end
+
+    def do_get
+      get :index
+    end
+
+    it "for index list it should have n harddisks" do
+      do_get
+      @harddisks = Harddisk.find(:all)
+      @harddisks.should have(4).items
+    end
+
+  end
+end
