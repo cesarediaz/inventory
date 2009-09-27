7c98a78a94a5d53c167c66a047a31d08f9c81728
César Enrique Díaz <cesare.diaz@gmail.com>
2009-09-27 13:01:00 -0300

    - Model   * added restriction to computer model where a     computer needs to has a place

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -16,7 +16,7 @@ class Computer < ActiveRecord::Base
   #################################################
   # VALIDATIONS
   validates_uniqueness_of :name, :ip, :mac
-  validates_presence_of :name, :ip, :mac
+  validates_presence_of :name, :ip, :mac, :place_id
   validates_size_of :ip, :within => 8..15
   validates_format_of :ip, :with => /^\b(?:\d{1,3}\.){3}\d{1,3}\b/i
   validates_format_of :mac, :with => /^([0-9a-f]{2}([:-]|$)){6}$/i
